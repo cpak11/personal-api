504868d64809460ceb81902f87ca21da1bba1d4c
cpak11 <c.pak81@gmail.com>
2017-09-26 01:44:05 -0400

    not quite finished...

--- controllers/main_ctrl.js
+++ controllers/main_ctrl.js
@@ -9,6 +9,8 @@ const getLocation = (req, res, next)=>{
 	res.json({location: user.location})
 }
 const getOccupations = (req, res, next)=>{
+	if(req.query.order === 'asc'){return user.occupations.sort()}
+	else if(req.query.order === 'desc'){return user.occupations.sort().reverse}
 	res.json({occupations: user.occupations})
 }
 const getOccupationsLatest = (req, res, next)=>{
@@ -20,7 +22,51 @@ const getHobbies = (req, res, next)=>{
 const getHobbiesType = (req, res, next)=>{	
 	res.json(user.hobbies.filter(hobbies=>hobbies.type===req.params.type))	
 }
+const getFamily = (req,res,next)=>{
+	if(parseInt(req.query.gender)){
+		var result=user.filter(function(uuser){
+			return uuser.gender === gender;
+		})
+		res.json(result)
+	}
+	return res.json({family: user.family})
+}
+const getRestaurants = (req,res,next)=>{
+	if(parseInt(req.query.restaurants)){
+		var result=user.filter(function(usr){
+			return usr.restaurants === restaurants;
+		})
+		res.json(result)
+	}	
+	return res.json({restaurants: user.restaurants});
+}
 
+
+const putName = function(req, res, next) {
+  user.name = req.body.name;
+  res.json({name: user.name});
+};
+const putLocation = function(req, res, next) {
+  user.location = req.body.location;
+  res.json({location: user.location});
+};
+const postHobbies = function(req, res, next) {
+  user.hobbies.push(req.body.hobby);
+  res.json({hobbies: user.hobbies});
+};
+
+const postOccupations = function(req, res, next) {
+  user.occupations.push(req.body.occupation);
+  res.json({occupations: user.occupations});
+};
+const postFamily = function(req, res, next) {
+  user.family.push(req.body.family);
+  res.json({family: user.family});
+};
+const postRestaurants = function(req, res, next) {
+  user.restaurants.push(req.body.restaurant);
+  res.json({restaurants: user.restaurants});
+};
 
 
 module.exports = {
@@ -29,5 +75,14 @@ module.exports = {
 	getOccupations,
 	getOccupationsLatest,
 	getHobbies,
-	getHobbiesType
+	getHobbiesType,
+	getFamily,
+	getRestaurants,
+	putName,
+	putLocation,
+	postHobbies,
+	postOccupations,
+	postFamily,
+	postRestaurants,
+	
 }
--- index.js
+++ index.js
@@ -19,8 +19,16 @@ app.get('/api/occupation', main_ctrl.getOccupations)
 app.get('/api/occupation/:latest', main_ctrl.getOccupationsLatest)
 app.get('/api/hobbies', main_ctrl.getHobbies)
 app.get('/api/hobbies/:type', main_ctrl.getHobbiesType)
-
-
+app.get('/api/family', main_ctrl.getFamily)
+app.get('/api/family/:gender', main_ctrl.getFamily)
+app.get('/api/restaurants', main_ctrl.getRestaurants)
+app.get('/api/restaurants/:name', main_ctrl.getRestaurants)
+app.put("/name", main_ctrl.putName);
+app.put("/location", main_ctrl.putLocation);
+app.post("/hobbies", main_ctrl.postHobbies);
+app.post("/occupations", main_ctrl.postOccupations);
+app.post("/family", main_ctrl.postFamily);
+app.post("/restaurants", main_ctrl.postRestaurants);
 
 
 
